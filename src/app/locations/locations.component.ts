b19ddf92878c5f1e63d1b19f7fb5e7c2e02b6990
Andres Gomez <andres.gomez@mercadoni.com>
2019-03-22 22:52:05 -0500

    Complete locations view

--- src/app/locations/locations.component.ts
+++ src/app/locations/locations.component.ts
@@ -1,4 +1,6 @@
+import { Location } from './../location';
 import { Component, OnInit } from '@angular/core';
+import { RicknmortyService } from '../ricknmorty.service';
 
 @Component({
   selector: 'app-locations',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./locations.component.scss']
 })
 export class LocationsComponent implements OnInit {
+  locations: Location[] = [];
+  show_more: boolean = false;
+  current_page: number = 1;
 
-  constructor() { }
+  constructor(private ricknmortyService: RicknmortyService) { }
 
   ngOnInit() {
+    this.getLocations();
+  }
+
+  getLocations(): void {
+    this.ricknmortyService.getLocations(this.current_page)
+      .subscribe(response => {
+        this.locations = this.locations.concat(response.results);
+        this.show_more = response.info.next !== "";
+      });
+  }
+
+  loadMore(): void {
+    this.current_page++;
+    this.getLocations();
   }
 
 }
